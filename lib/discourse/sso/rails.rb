d9823980473842ca2a39c0330db13d881dd0ac32
Egor Vorobiev <vorobievegoralexandrovich@gmail.com>
2015-04-20 11:58:58 +0300

    Initial commit

---
+++ lib/discourse/sso/rails.rb
@@ -0,0 +1,9 @@
+require "discourse/sso/rails/version"
+
+module Discourse
+  module Sso
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/discourse/sso/rails/version.rb
@@ -0,0 +1,7 @@
+module Discourse
+  module Sso
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
